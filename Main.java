378795886b47beae4864e32e453c6ce875fa43f8
Alexandr <alexbratchin@gmail.com>
2017-02-13 18:01:41 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+package ru.nkbpp;
+
+public class Main {
+
+    public static void main(String ... args) {
+        Boolean b = new Boolean("/true");
+        System.out.println(b);
+        System.out.println("mhgcvnhc");
+    }
+
+    public static void main(String args) {
+        Boolean b = new Boolean("/false");
+        System.out.println(b == false);
+    }
+}
